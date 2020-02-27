9fbe19519a3df0413188e02fac8c698fb548eea6
MooseLiu <moliu@paypal.com>
2020-02-27 18:34:08 +0800

    adding resources

---
+++ SpringBoot/gooseonroad/purejava/src/main/java/DataStructure/Testing.java
@@ -0,0 +1,21 @@
+package DataStructure;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Testing {
+    public static void main(String[] args) {
+
+    }
+
+    public void entrypoint() {
+        ArrayList<String> al = new ArrayList<>();
+        new ConcurrentHashMap<>();
+        Executors.newCachedThreadPool();
+        new ReentrantLock();
+//        new ThreadPoolExecutor();
+    }
+
+}
