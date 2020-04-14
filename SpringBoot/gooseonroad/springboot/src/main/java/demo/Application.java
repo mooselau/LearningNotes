421b861c6d4eff7ec4898306fcec32f329b34ec9
moliu <moliu@paypal.com>
2020-04-14 17:36:07 +0800

    Adding property read to @Value.

--- SpringBoot/gooseonroad/springboot/src/main/java/demo/Application.java
+++ SpringBoot/gooseonroad/springboot/src/main/java/demo/Application.java
@@ -1,9 +1,8 @@
 package demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.io.IOException;
@@ -16,9 +15,9 @@ import java.util.Properties;
 public class Application {
     public static void main(String[] args) {
         // 1. Use default SpringApplication to run
-//        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
         // 2. use ApplicationContext to run Spring IoC
-        ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
+//        ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
 //        UserService svr = context.getBean(UserService.class);
 //        svr.printUser(new User());
         // below simply want ApplicationContext could read application.properties
