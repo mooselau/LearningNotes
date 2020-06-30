ee01f5b533e4b540a4cedaff37b7e8d7a7ce61ab
moliu <moliu@paypal.com>
2020-06-30 14:20:01 +0800

    Adding IP & config tests

--- SpringBoot/gooseonroad/springboot/src/main/java/demo/properties/UserProperties.java
+++ SpringBoot/gooseonroad/springboot/src/main/java/demo/properties/UserProperties.java
@@ -13,9 +13,13 @@ public class UserProperties {
     @Value("${custom.property.num:18}")
     private Integer propertyNum;
 
+    @Value("${PROJECTS_HOME}")
+    private String envVar;
+
     @PostConstruct
     public void init() {
         System.out.println("=> propertyNum: " + propertyNum);
+        System.out.println("=> envVar: " + envVar);
     }
 
     public String getPropertyName() {
